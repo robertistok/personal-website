71c477cfd244aeda74217441f0e0cb81812e133f
robertistok <robertistok94@gmail.com>
2019-07-29 22:18:47 +0200

    Scaffold blog/contact

---
+++ src/pages/blog.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { Link, graphql, useStaticQuery } from "gatsby";
+
+import Bio from "../components/Bio";
+import Layout from "../components/Layout";
+import SEO from "../components/Seo";
+import { rhythm } from "../utils/typography";
+import useSiteMetadata from "../hooks/useSiteMetadata";
+
+const BlogIndex = props => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+        edges {
+          node {
+            excerpt
+            fields {
+              slug
+            }
+            frontmatter {
+              date(formatString: "MMMM DD, YYYY")
+              title
+              description
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const { title: siteTitle } = useSiteMetadata();
+
+  const posts = data.allMarkdownRemark.edges;
+
+  return (
+    <Layout location={props.location} title={siteTitle}>
+      <SEO title="All posts" />
+      {posts.map(({ node }) => {
+        const title = node.frontmatter.title || node.fields.slug;
+        return (
+          <div key={node.fields.slug}>
+            <h3
+              style={{
+                marginBottom: rhythm(1 / 4),
+              }}
+            >
+              <Link
+                style={{ boxShadow: "none" }}
+                to={`blog/${node.fields.slug}`}
+              >
+                {title}
+              </Link>
+            </h3>
+            <small>{node.frontmatter.date}</small>
+            <p
+              dangerouslySetInnerHTML={{
+                __html: node.frontmatter.description || node.excerpt,
+              }}
+            />
+          </div>
+        );
+      })}
+    </Layout>
+  );
+};
+
+export default BlogIndex;
