34aedfbb879585372b9c6e2844c2f9f32cecbf7e
robertistok <robertistok94@gmail.com>
2019-07-30 21:54:11 +0200

    Fix link to point to /blog

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -1,24 +1,21 @@
-import React from "react"
-import { Link, graphql } from "gatsby"
+import React from "react";
+import { Link, graphql } from "gatsby";
 
-import Bio from "../components/Bio"
-import Layout from "../components/Layout"
-import SEO from "../components/Seo"
-import useSiteMetadata from "../hooks/useSiteMetadata"
-import { rhythm, scale } from "../utils/typography"
+import Bio from "../components/Bio";
+import Layout from "../components/Layout";
+import SEO from "../components/Seo";
+import useSiteMetadata from "../hooks/useSiteMetadata";
+import { rhythm, scale } from "../utils/typography";
 
 const BlogPostTemplate = props => {
-  const post = props.data.markdownRemark
+  const post = props.data.markdownRemark;
 
-  const { previous, next } = props.pageContext
-  const { title: siteTitle } = useSiteMetadata()
+  const { previous, next } = props.pageContext;
+  const { title: siteTitle } = useSiteMetadata();
 
   return (
     <Layout location={props.location} title={siteTitle}>
-      <SEO
-        title={post.frontmatter.title}
-        description={post.frontmatter.description || post.excerpt}
-      />
+      <SEO title={post.frontmatter.title} />
       <h1
         style={{
           marginTop: rhythm(1),
@@ -55,24 +52,24 @@ const BlogPostTemplate = props => {
       >
         <li>
           {previous && (
-            <Link to={previous.fields.slug} rel="prev">
+            <Link to={"blog" + previous.fields.slug} rel="prev">
               ← {previous.frontmatter.title}
             </Link>
           )}
         </li>
         <li>
           {next && (
-            <Link to={next.fields.slug} rel="next">
+            <Link to={"blog" + next.fields.slug} rel="next">
               {next.frontmatter.title} →
             </Link>
           )}
         </li>
       </ul>
     </Layout>
-  )
-}
+  );
+};
 
-export default BlogPostTemplate
+export default BlogPostTemplate;
 
 export const pageQuery = graphql`
   query BlogPostBySlug($slug: String!) {
@@ -87,4 +84,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
