b221cf0e3c3a52b3537eb80aefbcb20d05079cec
robertistok <robertistok94@gmail.com>
2019-08-04 17:24:55 +0200

    Update Social.tsx

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -1,10 +1,20 @@
 import React from "react";
-import { FaLinkedin, FaTwitter, FaGithub, FaMedium } from "react-icons/fa";
+import {
+  FaLinkedin,
+  FaTwitter,
+  FaGithub,
+  FaMedium,
+  FaGoodreadsG,
+} from "react-icons/fa";
 import styled from "styled-components";
 
 import useSiteMetadata from "../hooks/useSiteMetadata";
 
 const LINKEDIN = "https://www.linkedin.com/in";
+const GITHUB = "https://github.com";
+const MEDIUM = "https://medium.com";
+const TWITTER = "https://twitter.com";
+const GOODREADS = "https://goodreads.com";
 
 const Social: React.FunctionComponent = (): React.ReactElement => {
   const {
@@ -13,19 +23,28 @@ const Social: React.FunctionComponent = (): React.ReactElement => {
 
   return (
     <Root>
-      <OutsideLink href={`${LINKEDIN}/${social.linkedin}`}>
+      <OutsideLink href={`${LINKEDIN}/${social.linkedin}`} title="LinkedIn">
         <FaLinkedin />
       </OutsideLink>
-      <FaGithub />
-      <FaMedium />
-      <FaTwitter />
+      <OutsideLink href={`${GITHUB}/${social.github}`} title="GitHub">
+        <FaGithub />
+      </OutsideLink>
+      <OutsideLink href={`${MEDIUM}/@${social.medium}`} title="Medium">
+        <FaMedium />
+      </OutsideLink>
+      <OutsideLink href={`${TWITTER}/${social.twitter}`} title="Twitter">
+        <FaTwitter />
+      </OutsideLink>
+      <OutsideLink href={`${GOODREADS}/${social.goodreads}`} title="Goodreads">
+        <FaGoodreadsG />
+      </OutsideLink>
     </Root>
   );
 };
 
 const Root = styled.div`
   display: grid;
-  grid-template-columns: repeat(4, 30px);
+  grid-template-columns: repeat(5, 30px);
   grid-gap: 10px;
 
   svg {
