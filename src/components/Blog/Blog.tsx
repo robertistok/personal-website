ccc1e564824287e660b05566b3885d51c2cb6af8
robertistok <robertistok94@gmail.com>
2019-08-04 14:56:20 +0200

    Add short intro to blog

--- src/components/Blog/Blog.tsx
+++ src/components/Blog/Blog.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import styled from "styled-components";
 import { graphql, useStaticQuery } from "gatsby";
+import Image from "gatsby-image";
 
 import PostCard from "./PostCard";
 import { rhythm } from "../../utils/typography";
+import { device } from "../../styles/constants";
+import { File, MarkdownRemarkConnection } from "../../generated/graphql-types";
+import useSiteMetadata from "../../hooks/useSiteMetadata";
 
 const Blog: React.FunctionComponent = (): React.ReactElement => {
-  const data = useStaticQuery(graphql`
+  const data: {
+    avatar: File;
+    allMarkdownRemark: MarkdownRemarkConnection;
+  } = useStaticQuery(graphql`
     query {
       allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
         edges {
@@ -23,12 +30,29 @@ const Blog: React.FunctionComponent = (): React.ReactElement => {
           }
         }
       }
+
+      avatar: file(absolutePath: { regex: "/profile-pic.png/" }) {
+        childImageSharp {
+          fixed(width: 50, height: 50) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
     }
   `);
+  const { author } = useSiteMetadata();
+
   const posts = data.allMarkdownRemark.edges;
 
   return (
     <Root>
+      <Header>
+        <StyledImage
+          fixed={data.avatar.childImageSharp.fixed}
+          alt={author.name}
+        />
+        <h5>Personal blog about coding, remote work, habits and much more</h5>
+      </Header>
       {posts.map(({ node }) => {
         const title = node.frontmatter.title || node.fields.slug;
         return (
@@ -50,6 +74,31 @@ const Root = styled.section`
   flex-direction: column;
   margin: auto;
   max-width: ${rhythm(24)};
+`;
+
+const Header = styled.section`
+  display: grid;
+  grid-template-columns: 50px auto;
+  grid-gap: ${rhythm(1)};
+  align-items: center;
+  margin-bottom: ${rhythm(1)};
+
+  h5 {
+    margin: 0;
+  }
+`;
+
+const StyledImage = styled(Image)`
+  margin-bottom: 0;
+  justify-self: center;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+
+  @media ${device.tablet} {
+    grid-row: auto;
+    justify-self: flex-end;
+  }
 `;
 
 export default Blog;
