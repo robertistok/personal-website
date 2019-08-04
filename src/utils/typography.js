ed367464a1891dbb2282bed079df9885ef83ba73
robertistok <robertistok94@gmail.com>
2019-08-04 14:02:02 +0200

    Change typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,21 +1,19 @@
-import Typography from "typography"
-import Wordpress2016 from "typography-theme-wordpress-2016"
+import Typography from "typography";
+import doelger from "typography-theme-doelger";
 
-Wordpress2016.overrideThemeStyles = () => ({
-  "a.gatsby-resp-image-link": {
-    boxShadow: "none",
+doelger.overrideThemeStyles = () => ({
+  a: {
+    backgroundImage: "none",
   },
-})
+});
 
-delete Wordpress2016.googleFonts
-
-const typography = new Typography(Wordpress2016)
+const typography = new Typography(doelger);
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== "production") {
-  typography.injectStyles()
+  typography.injectStyles();
 }
 
-export default typography
-export const { rhythm } = typography
-export const { scale } = typography
+export default typography;
+export const { rhythm } = typography;
+export const { scale } = typography;
