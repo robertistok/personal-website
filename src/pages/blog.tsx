0a0e71e563db7bb226739680a16d136260d17ca9
robertistok <robertistok94@gmail.com>
2019-08-11 12:18:39 +0200

    Add transition to the blog page

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { GatsbyLocation } from "local-types";
+import { useTransition, animated } from "react-spring";
 
 import Layout from "../components/Layout";
 import Blog from "../components/Blog";
@@ -14,11 +15,27 @@ const BlogIndex: React.FunctionComponent<BlogIndexProps> = ({
   location,
 }): React.ReactElement => {
   const { title: siteTitle } = useSiteMetadata();
+  const { prevPath = "" } = location.state;
+
+  const comingBack = prevPath.match(/\/blog\/*/);
+
+  const transitions = useTransition(location, location => location.pathname, {
+    from: {
+      opacity: 0.5,
+      transform: `translate3d(${comingBack ? "-100vw" : "100vw"}, 0, 0)`,
+    },
+    enter: { opacity: 1, transform: "translate3d(0, 0, 0)" },
+    config: { tension: 280, friction: 40 },
+  });
 
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="All posts" />
-      <Blog location={location} />
+      {transitions.map(({ props, key }) => (
+        <animated.div key={key} style={props}>
+          <Blog location={location} />
+        </animated.div>
+      ))}
     </Layout>
   );
 };
